bdcd2af03ad07bfdc62e48cf6981a9d0e2be185a
samrin-subha <syedasamrinsubha@gmail.com>
2026-05-07 02:57:12 -0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'developer_profile.dart';
+import 'settings_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DeveloperProfile(),
+      home: const SettingsPage(),
     );
   }
 }
